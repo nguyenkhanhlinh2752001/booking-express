d55faaa78048d217b9704ef81eaf0aa4ca886317
linhnk <nguyenkhanhlinh2752001@gmail.com>
2022-08-31 23:36:52 +0700

    Add jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js" 
 import bcrypt from 'bcrypt'
 import { createError } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 export const register = async ( req, res ) => {
     try {
@@ -31,6 +32,9 @@ export const login = async ( req, res, next) => {
         
         const isCorrectPassword = await bcrypt.compare( req.body.password, user.password )
         if ( !isCorrectPassword ) return next( createError( 404, "Wrong password or username" ) )
+
+        const token = jwt.sign( { id: user._id, isAdmin: user.isAdmin }, )
+        
 
         const { password, isAdmin, ...otherDetails } = user._doc
            
